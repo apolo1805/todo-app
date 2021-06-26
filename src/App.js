1405e23e8ae1526d37f41dba145452070ae6d876
elad <eladlevavi@gmail.com>
2021-06-26 13:30:52 +0300

    App header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+       <h1>Tasks manager web application</h1>
+       <h2>This web app manages the daily tasks need to be done.</h2>
       </header>
     </div>
   );
